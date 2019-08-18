70a313fde46ff5fccbb2dacb42589cc0b0a52e29
unknown <vinicius.silveira8@hotmail.com>
2019-08-18 19:10:09 -0300

    Continuação da aplicação Mobile Apresentação dos cards, realização do match

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
 import {
   KeyboardAvoidingView,
   StyleSheet,
@@ -16,14 +17,22 @@ import logo from "../assets/logo.png";
 export default function Login({ navigation }) {
   const [user, setUser] = useState("");
 
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user => {
+      if (user) {
+        navigation.navigate("Main", { user });
+      }
+    });
+  }, []);
+
   async function handleLogin() {
     const response = await api.post("/devs", { username: user });
 
     const { _id } = response.data;
 
-    console.log(_id);
+    await AsyncStorage.setItem("user", _id);
 
-    navigation.navigate("Main", { _id });
+    navigation.navigate("Main", { user: _id });
   }
 
   return (
